1dbb8d755c1b7cd8c843efca9d4645ebe3b8d1f2
BG-Rinkitori <minhssjb@gmail.com>
2025-09-12 20:02:31 +0700

    Update c511100000.lua

--- scripts/c511100000.lua
+++ scripts/c511100000.lua
@@ -2,36 +2,48 @@
 --Darkness Restrain
 local s,id=GetID()
 function s.initial_effect(c)
-	--When your opponent normal/special summons a monster, change all monster to face-down def position
+	--When your opponent normal/special summons a monster, change all monsters to face-down def position
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_POSITION)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_SUMMON_SUCCESS)
-	e1:SetProperty(EFFECT_FLAG_DELAY)
 	e1:SetCondition(s.condition)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e2)
+end
 
-end
---check if opponent summon a monster level 7 or higher, and that card can be set/change position and not a maximum mode
+-- opponent summoned monster check
 function s.filter1(c,tp)
-	return c:IsSummonPlayer(1-tp) and c:IsLevelAbove(7) and c:IsFaceup() and c:IsLocation(LOCATION_MZONE)
+	return c:IsSummonPlayer(1-tp) and c:IsLevelAbove(7) and c:IsFaceup() and c:IsAttackPos()
 end
-function s.filter(c)
-	return c:IsRace(RACE_SPELLCASTER)and c:IsAttribute(ATTRIBUTE_DARK) and c:IsFaceup()
+
+-- your DARK Spellcaster
+function s.filter_self(c)
+	return c:IsRace(RACE_SPELLCASTER) and c:IsAttribute(ATTRIBUTE_DARK) and c:IsFaceup()
 end
+
+-- opponent’s monsters to be flipped
+function s.filter_op(c)
+	return c:IsAttackPos() and c:IsCanTurnSet()
+end
+
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	return eg:IsExists(s.filter1,1,nil,tp) and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_MZONE,0,1,nil)
+	return eg:IsExists(s.filter1,1,nil,tp)
+		and Duel.IsExistingMatchingCard(s.filter_self,tp,LOCATION_MZONE,0,1,nil)
 end
+
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,0,LOCATION_MZONE,1,nil) end
-	local g=Duel.GetMatchingGroup(s.filter,tp,0,LOCATION_MZONE,nil)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter_op,tp,0,LOCATION_MZONE,1,nil) end
+	local g=Duel.GetMatchingGroup(s.filter_op,tp,0,LOCATION_MZONE,nil)
 	Duel.SetOperationInfo(0,CATEGORY_POSITION,g,#g,0,0)
 end
-	--change all opponent's monsters to face-down def position
+
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetMatchingGroup(s.filter,tp,0,LOCATION_MZONE,nil)
+	local g=Duel.GetMatchingGroup(s.filter_op,tp,0,LOCATION_MZONE,nil)
 	if #g>0 then
 		Duel.ChangePosition(g,POS_FACEDOWN_DEFENSE)
 	end
